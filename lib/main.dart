1388c4082130ca15b31f44149ea3018306af3a62
Federico Mastrini <federicomastrini93@gmail.com>
2020-01-20 00:27:55 +0100

    New colors

--- lib/main.dart
+++ lib/main.dart
@@ -10,14 +10,12 @@ class MyApp extends StatelessWidget {
       title: 'Fedemas',
       theme: ThemeData(
         buttonColor: Color.fromARGB(255, 54, 124, 255),
-        primarySwatch: Colors.blue,
         accentColor: Color.fromARGB(255, 54, 124, 255),
-        canvasColor: Color.fromARGB(255, 251, 252, 253),
+        canvasColor: const Color(0xFF191919),//Color.fromARGB(255, 251, 252, 253),
         textTheme: Theme.of(context).textTheme.apply(
-          bodyColor: Color.fromARGB(255, 51, 70, 128),
+          bodyColor: const Color(0xFFAAAAAA),
           displayColor: Color.fromARGB(255, 51, 70, 128),
-          fontFamily: 'Lato',
-        )
+        ),
       ),
       home: HomeScreen(),
     );
